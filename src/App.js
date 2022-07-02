1d775e6b579437261ac6dd8678681c0c53660acb
Volg <lost-is-died@mail.ru>
2022-07-02 16:32:02 +0500

    Изменены технические неполадки

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import MainLayout from "./components/MainLayout/MainLayout";
+import './styles/main.scss'
 
 const App = () => {
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<MainLayout />}>
-                    <Route index element={<div>Главная страница статей</div>}></Route>
-                    <Route path="posts" element={<div>Главная страница статей</div>}></Route>
-                    <Route path="posts/:id" element={<div>Одна статья</div>}></Route>
-                    <Route path="add-post" element={<div>Добавление поста</div>}></Route>
-                    <Route path="login" element={<div>Логин</div>}></Route>
-                    <Route path="register" element={<div>Регистрация</div>}></Route>
+                    <Route index element={<div>Главная страница статей</div>}/>
+                    <Route path="posts" element={<div>Главная страница статей</div>}/>
+                    <Route path="posts/:id" element={<div>Одна статья</div>}/>
+                    <Route path="add-post" element={<div>Добавление поста</div>}/>
+                    <Route path="login" element={<div>Логин</div>}/>
+                    <Route path="register" element={<div>Регистрация</div>}/>
                 </Route>
                 <Route path="*" element={<div>404 - страница не найдена</div>}/>
             </Routes>
--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import style from './index.module.scss'
 import {Outlet} from "react-router-dom";
-<<<<<<< HEAD
-=======
 import Header from "../Header/Header";
->>>>>>> header-component
-
 
 const MainLayout = (props) => {
     return (
         <section className={style.mainLayout}>
-            <Header/>
+            <Header />
             <Outlet />
         </section>
     );
